b85d202cd7a1152fff82715223dba75465aeae36
mahdi <mahdi.zxzxz@gmail.com>
2023-06-28 13:27:08 +0330

    aghdasie fix now fixing singin

--- src/Vidslider.jsx
+++ src/Vidslider.jsx
@@ -72,7 +72,7 @@ const Vidslider = (propsvid) => {
         </div>
     </div>
     <div class="login-area">
-      <Link to='http://localhost:3000/Singin' className='lgn'>بریم باهم ثبت نام کنیم</Link>
+      <Link to='./Singin' className='lgn'>بریم باهم ثبت نام کنیم</Link>
     </div>
        </>
       );
